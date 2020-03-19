286219dd1a2ec30c0c66461a8a2d6b3a63c652a4
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2020-03-19 16:44:43 +0000

    Updates

---
+++ precache-manifest.dcaf41eab65c97006b0328ce8ebcf499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e43001f2e40079c4cc4d47f5880c92",
+    "url": "/react-redux-refreshement/index.html"
+  },
+  {
+    "revision": "07a99808f5311c25dad8",
+    "url": "/react-redux-refreshement/static/js/2.8c9e4bbb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-redux-refreshement/static/js/2.8c9e4bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002e1e63f527d52cfa07",
+    "url": "/react-redux-refreshement/static/js/main.d3ec2334.chunk.js"
+  },
+  {
+    "revision": "5673748e11a1ab95a65f",
+    "url": "/react-redux-refreshement/static/js/runtime-main.f1529dc1.js"
+  }
+]);
